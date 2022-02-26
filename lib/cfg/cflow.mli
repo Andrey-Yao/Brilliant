81cdda207873f67378122ee29c212a5e0703076d
Andrey Yao <awy32@cornell.edu>
2022-02-25 20:35:18 -0500

    hehe

--- lib/cfg/cflow.mli
+++ lib/cfg/cflow.mli
@@ -1,23 +1,24 @@
-open! Core
 open Ir
-module SM = String.Map
-module G = Graph
-
-type block_t = string * Instr.t Array.t
-(**[(block_name, instrs)]*)
+open Util
 
 (**[Next] is fall through*)
 type edge = True | False | Jump | Next
 
+module G: Sig.G with type e = edge
+
+type block_t = string * Instr.t Array.t
+(**[(block_name, instrs)]*)
+
 type t = {
-  graph : edge G.t; (*The control flow graph*)
+  graph : G.t; (*The control flow graph*)
   args : Instr.dest list;
   order : string list; (*Blocks in original order*)
   ret_type : Bril_type.t option;
   func_name : string; (*Name of function this cfg represents*)
-  map : block_t String.Map.t; (*yeah*)
+  map : block_t SM.t; (*yeah*)
 }
 
 val of_func : Func.t -> t
 val to_func : t -> Func.t
-val to_dot : names_only:bool -> Out_channel.t -> t -> unit
+val to_dot : names_only:bool -> Stdio.Out_channel.t -> t -> unit
+                                                      
--- lib/cfg/graph.mli
+++ lib/cfg/graph.mli
@@ -1,26 +1,5 @@
-open! Core
-
 (**This is a module representing digraphs*)
 
-type 'a t = {
-    succs_map : ('a * string) list String.Map.t;
-    preds_map : ('a * string) list String.Map.t;
-}
-
-(**adds edge [e]. Creates [src] and [dst] nodes if missing*)
-val add_edge : 'a t -> src:string -> dst:string -> 'a -> 'a t
-
-val remove : 'a t -> string -> 'a t
-val succs_e : 'a t -> string -> ('a * string) list
-val preds_e : 'a t -> string -> ('a * string) list
-val succs : 'a t -> string -> string list
-val preds : 'a t -> string -> string list
-
-(**[empty] makes an empty graph*)
-val empty : 'a t
-
-(**Generic function for outputting graph to graphviz form.*)
-val to_dot : oc:Out_channel.t ->  nodes:(string list) ->
-             label:string -> ?nf:(string -> string)
-             -> ?ef:(string -> 'a -> string -> string)
-             -> 'a t -> unit
+module Make(Edg: Sig.E) : Sig.G
+       with type e = Edg.t
+        and type ES.elt = Edg.t * string
